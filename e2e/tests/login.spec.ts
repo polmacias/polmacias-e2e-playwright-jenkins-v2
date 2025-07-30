06498d112ee3f4919bd56bc52227afaf33874e3d
POLO <polmaciaslopez08@gmail.com>
2025-07-30 21:36:16 +0200

    feat: Add new buy and register flows, fix login test, and ensure repository cleanliness with gitignore

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -7,39 +7,36 @@ config({ path: './pass.env' });
 const BASE_URL = process.env.BASE_URL as string;
 const USER_EMAIL = process.env.EMAIL as string;
 const USER_PASSWORD = process.env.PASSWORD as string;
-const USER_USERNAME = process.env.USERNAME as string;
+const TEST_USERNAME = process.env.TEST_USERNAME as string;
 
 
 test.describe('Login Flow', () => {
 
-  test('should allow a user to login successfully', async ({ page }) => {
+    test('should allow a user to login successfully', async ({ page }) => {
 
-    await test.step('1. Navigate to the login page and consent cookies.', async () => {
-      await page.goto(BASE_URL);
-      await page.getByRole('button', { name: 'Consent' }).click();
+        await test.step('1. Navigate to the login page and consent cookies.', async () => {
+            await page.goto(BASE_URL);
+            await page.getByRole('button', { name: 'Consent' }).click();
+        });
+
+        await test.step('2. Click on "Signup / Login" button.', async () => {
+            await page.getByRole('link', { name: ' Signup / Login' }).click();
+
+            await expect(page.getByText('Login to your account')).toBeVisible();
+        });
+
+        await test.step('3. Fill in login credentials.', async () => {
+            await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(USER_EMAIL);
+            await page.getByRole('textbox', { name: 'Password' }).fill(USER_PASSWORD);
+        });
+
+        await test.step('4. Click the Login button.', async () => {
+            await page.getByRole('button', { name: 'Login' }).click();
+        });
+
+        await test.step('5. Verify successful login.', async () => {
+            await expect(page.locator('#header')).toContainText(`Logged in as ${TEST_USERNAME}`);
+            //await expect(page).toHaveURL(BASE_URL);
+        });
     });
-
-    await test.step('2. Click on "Signup / Login" button.', async () => {
-      await page.getByRole('link', { name: ' Signup / Login' }).click();
-
-      await expect(page.getByText('Login to your account')).toBeVisible();
-    });
-
-    await test.step('3. Fill in login credentials.', async () => {
-      await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(USER_EMAIL);
-      await page.getByRole('textbox', { name: 'Password' }).fill(USER_PASSWORD);
-    });
-
-    await test.step('4. Click the Login button.', async () => {
-      await page.getByRole('button', { name: 'Login' }).click();
-    });
-
-    // await test.step('5. Verify successful login.', async () => {
-    //   await expect(page.getByText(`Logged in as ${USER_USERNAME}`)).toBeVisible();
-    //   await expect(page).toHaveURL(BASE_URL);
-    // });
-
-  });
-
 });
-
